65ae5b7ddaffa4788e4e01d7664a2a9a3f4e6a7c
tungntd00605 <tungntd00605@fpt.edu.vn>
2018-03-31 18:39:32 +0700

    Khoi tao project assignment

---
+++ assignment-1708e/src/mypack/Account.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypack;
+
+/**
+ *
+ * @author TVD1104
+ */
+public class Account {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account(String name) {
+        this.name = name;
+    }
+    
+}
